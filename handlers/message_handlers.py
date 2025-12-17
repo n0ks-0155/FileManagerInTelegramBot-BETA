8bc9a965dee044ee6e1142a3b162cd304c7fa4dc
n0ks-0155 <4t3w35g@gmail.com>
2025-12-17 12:18:38 +1000

    message_handlers.py

--- handlers/message_handlers.py
+++ handlers/message_handlers.py
@@ -2,7 +2,7 @@ from telebot.types import Message
 from utils.data_manager import load_data, save_data, init_user
 from utils.navigation import navigate_to_path
 import telebot
-import uuid  # Для генерации уникальных short_id
+import uuid 
 import logging
 
 logger = logging.getLogger(__name__)
@@ -14,26 +14,25 @@ def register_message_handlers(bot: telebot.TeleBot):
         data = load_data()
         init_user(data, user_id)
 
-        # Проверяем, что это не команда
         if message.content_type == 'text' and message.text.startswith('/'):
             return
 
         current = navigate_to_path(data["users"][user_id]["structure"], data["users"][user_id]["current_path"])
 
         if message.content_type == 'text':
-            # Сохраняем текст как файл типа 'text'
+            #Сохраняем текст как файл типа 'text'
             current["files"].append({"type": "text", "content": message.text})
             save_data(data)
             bot.reply_to(message, "Текстовое сообщение сохранено в текущей папке.")
         elif message.content_type == 'document':
-            # Сохраняем документ
+            #Сохраняем документ
             file_id = message.document.file_id
             short_id = uuid.uuid4().hex[:8]
             current["files"].append({"type": "document", "file_id": file_id, "file_name": message.document.file_name, "short_id": short_id})
             data["users"][user_id]["file_mappings"][short_id] = file_id
             save_data(data)
             bot.reply_to(message, "Документ сохранён в текущей папке.")
-        # Аналогично для фото, видео и аудио
+        #Аналогично для фото, видео и аудио
         elif message.content_type == 'photo':
             file_id = message.photo[-1].file_id
             short_id = uuid.uuid4().hex[:8]
@@ -54,4 +53,5 @@ def register_message_handlers(bot: telebot.TeleBot):
             current["files"].append({"type": "audio", "file_id": file_id, "short_id": short_id})
             data["users"][user_id]["file_mappings"][short_id] = file_id
             save_data(data)
+
             bot.reply_to(message, "Аудио сохранено в текущей папке.")
